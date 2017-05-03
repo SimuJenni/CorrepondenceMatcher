b4f756592c0d6d7b0af0b599d419481ab0ab89f0
SimuJenni <simujenni@gmail.com>
2017-05-03 15:33:42 +0200

    bug fixing

--- Preprocessor.py
+++ Preprocessor.py
@@ -9,7 +9,7 @@ slim = tf.contrib.slim
 
 
 class Preprocessor:
-    def __init__(self, target_shape, augment_color=False, aspect_ratio_range=(0.9, 1.1), area_range=(0.9, 1.0),
+    def __init__(self, target_shape, augment_color=False, aspect_ratio_range=(0.7, 1.3), area_range=(0.2, 1.0),
                  im_shape=None):
         self.target_shape = target_shape
         self.augment_color = augment_color
