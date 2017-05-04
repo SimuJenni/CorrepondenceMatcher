b97c93d481eb34e04fc2efc34ac09724548ed107
SimuJenni <simujenni@gmail.com>
2017-05-04 12:50:48 +0200

    bug fixing

--- train_CMnet.py
+++ train_CMnet.py
@@ -4,9 +4,9 @@ from CMNetTrainer import CMNetTrainer
 from Preprocessor import Preprocessor
 
 
-model = CMNet(num_layers=5, batch_size=64)
+model = CMNet(num_layers=5, batch_size=48)
 data = ImageNet()
 preprocessor = Preprocessor(target_shape=[224, 224, 3], augment_color=True, im_shape=data.im_shape)
-trainer = CMNetTrainer(model=model, num_roi=4, dataset=data, pre_processor=preprocessor, num_epochs=10, tag='first_attempt',
+trainer = CMNetTrainer(model=model, num_roi=4, dataset=data, pre_processor=preprocessor, num_epochs=10, tag='second_attempt',
                        lr_policy='const', optimizer='adam')
 trainer.train()
